647a4619184c68ce55758bc8c8c1f0bc50a53364
hari <harikrishna266@gmail.com>
2024-04-23 16:59:33 +0530

    refactor: 💡 refactoring expressjs

--- custom-plugins/fictive_studios/admin/builder-2d/builder-2d.js
+++ custom-plugins/fictive_studios/admin/builder-2d/builder-2d.js
@@ -1,47 +1,35 @@
 import {Stage2D} from 'https://unpkg.com/@brocha-libs/builder-2d@33.2.0-1/index.mjs';
+import { Alpine } from "https://fictivecodes.com/scripts/alphine.esm.js";
 let rect;
-async function loadStage() {
-    const stage = new Stage2D();
-    stage.initializeStage();
-    rect = stage.createShape('rect'); 
-    await rect.setAttrs({x: 0});
-    stage.addToLayer(rect);
-    await rect.setAttrs({x: 1})
-}
+// async function loadStage() {
+//     const stage = new Stage2D();
+//     stage.initializeStage();
+//     rect = stage.createShape('rect');
+//     await rect.setAttrs({x: 0});
+//     stage.addToLayer(rect);
+//     await rect.setAttrs({x: 1})
+// }
+//
+// function createHolder() {
+//     const builderHolder = document.createElement( 'div' );
+//     builderHolder.setAttribute('hx-trigger', "load");
+//     builderHolder.setAttribute('hx-get', "https://fictivecodes.com");
+//     builderHolder.setAttribute('class', "bg-black hidden absolute inset-0 flex w-100 h-100 top-0 right-0 bottom-0 left-0");
+//     builderHolder.style.zIndex = 100001;
+//     wpcontentElement.parentNode.insertBefore( builderHolder, wpcontentElement.nextSibling );
+// }
+// var wpcontentElement = document.getElementById( 'wpcontent' );
+// if ( wpcontentElement ) {
+//     createHolder()
+// }
+//
+// await loadStage();
 
-function createHolder() {
-    const builderHolder = document.createElement( 'div' );
-    builderHolder.setAttribute('hx-trigger', "load");
-    builderHolder.setAttribute('hx-get', "https://fictivecodes.com");
-    builderHolder.setAttribute('class', "bg-black  absolute inset-0 flex w-100 h-100 top-0 right-0 bottom-0 left-0");
-    builderHolder.style.zIndex = 100001;
-    wpcontentElement.parentNode.insertBefore( builderHolder, wpcontentElement.nextSibling );
-}
-var wpcontentElement = document.getElementById( 'wpcontent' );
-if ( wpcontentElement ) {
-    createHolder()
-}
 
-await loadStage();
-
-document.querySelector('#serializeShapes').addEventListener('click', saveSerializedData)
-
-function saveSerializedData() {
-    var xhr = new XMLHttpRequest();
-    var templateName = document.getElementById('template-name');
-    
-    xhr.open('POST', ajaxurl, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    var data = {
-        action: 'save_template_action',
-        data: JSON.stringify(rect.serialize()),
-        name: templateName.value
-    };
-
-    var formData = Object.keys(data).map(function(key) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
-    }).join('&');
-
-    xhr.send(formData);
-}
+Alpine.data("editor", () => ({
+    count: 1,
+    increment() {
+        this.count = this.count + 1;
+    },
+}));
+Alpine.start();
