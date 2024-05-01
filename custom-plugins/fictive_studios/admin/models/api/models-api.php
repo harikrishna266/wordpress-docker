b2840b41d3ad7019c2eb16b418855cb4352e5feb
krishnajith <krishnajithr55@gmail.com>
2024-05-01 22:32:19 +0530

    fix: 🐛 imporoved data query for models
    ✅ Closes: 32

--- custom-plugins/fictive_studios/admin/models/api/models-api.php
+++ custom-plugins/fictive_studios/admin/models/api/models-api.php
@@ -25,42 +25,24 @@ class ModelsAPIAdmin
             exit;
         }
 
-        $query = $wpdb->prepare(
-            "SELECT m.*, c.ID AS print_area_model_coordinate_id, c.x_coordinate, c.y_coordinate, c.camera_x_coordinates,c.camera_y_coordinates, pa.ID AS print_area_id, pa.height, pa.width
-                FROM $models_table m 
-                LEFT JOIN $coordinates_table c ON m.ID = c.models_id
-                LEFT JOIN $print_areas_table pa ON c.print_area_id = pa.ID
-                WHERE m.ID = %d",
+        $models_query = $wpdb->prepare(
+            "SELECT * FROM $models_table WHERE id = %d",
             $id
         );
+        $model = $wpdb->get_row($models_query, OBJECT);
 
-        $results = $wpdb->get_results($query, ARRAY_A);
+        $print_area_coordinate_query = $wpdb->prepare(
+            "SELECT cot.*, pat.*
+             FROM $coordinates_table cot
+             LEFT JOIN $print_areas_table pat ON cot.print_area_id = pat.ID
+             WHERE cot.models_id = %d",
+            $model->ID
+        );
+        $print_area_coordinates = $wpdb->get_results($print_area_coordinate_query, ARRAY_A);
 
-        $models_with_coordinates = array();
-        foreach ($results as $row) {
-            $model_id = $row['ID'];
-            if (!isset($models_with_coordinates[$model_id])) {
-                $models_with_coordinates[$model_id] = array(
-                    'ID' => $row['ID'],
-                    'name' => $row['name'],
-                    'user' => $row['user'],
-                    'print_areas' => array(),
-                );
-            }
-            if (!empty($row['print_area_model_coordinate_id'])) {
-                $models_with_coordinates[$model_id]['print_areas'][] = array(
-                    'ID' => $row['print_area_model_coordinate_id'],
-                    'print_area_height' => $row['height'],
-                    'print_area_width' => $row['width'],
-                    'x_coordinate' => $row['x_coordinate'],
-                    'y_coordinate' => $row['y_coordinate'],
-                    'camera_x_coordinates' => $row['camera_x_coordinates'],
-                    'camera_y_coordinates' => $row['camera_y_coordinates'],
-                );
-            }
-        }
-        $models_list = array_values($models_with_coordinates);
-        echo json_encode($models_list);
+        $model->print_areas = $print_area_coordinates;
+        
+        echo json_encode($model);
         wp_die();
     }
 }
