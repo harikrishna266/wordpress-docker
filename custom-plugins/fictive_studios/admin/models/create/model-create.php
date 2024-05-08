3b72656b528b332c8f666dfc662a0a2d92078684
krishnajith <krishnajithr55@gmail.com>
2024-05-08 12:58:59 +0530

    fix: 🐛 added model drop down
    added model drop down to model create
    
    ✅ Closes: #41

--- custom-plugins/fictive_studios/admin/models/create/model-create.php
+++ custom-plugins/fictive_studios/admin/models/create/model-create.php
@@ -6,6 +6,16 @@ class ModelCreate
     public function get_model_create_template()
     {
         $action = ($_GET['action']);
+        $models_dummy_data = array(
+            array(
+                'name' => 'model_1',
+                'model_url' => 'model_1_url',
+            ),
+            array(
+                'name' => 'model_2',
+                'model_url' => 'model_2_url',
+            )
+        );
         require_once (plugin_dir_path(__FILE__) . 'model-create-view.php');
     }
 }
