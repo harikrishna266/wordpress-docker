96df70c279e5fc6a6d5c80638e50df251329095d
krishnajith <krishnajithr55@gmail.com>
2024-05-27 14:11:25 +0530

    feat: 🎸 changed design layer to text
    ✅ Closes: #57

--- custom-plugins/fictive_studios/admin/fashion-designs/create/fashion-design-create-view.php
+++ custom-plugins/fictive_studios/admin/fashion-designs/create/fashion-design-create-view.php
@@ -28,27 +28,27 @@
             </tr>
             <tr class="form-field">
                 <th scope="row"><label for="fashion_design_layer_1">Upload Layer 1*:</label></th>
-                <td><input type="file" id="fashion_design_layer_1" name="fashion_design_layer_1" required>
+                <td><input type="text" id="fashion_design_layer_1" name="fashion_design_layer_1" required>
                 </td>
             </tr>
             <tr class="form-field">
                 <th scope="row"><label for="fashion_design_layer_2">Upload Layer 2:</label></th>
-                <td><input type="file" id="fashion_design_layer_2" name="fashion_design_layer_2">
+                <td><input type="text" id="fashion_design_layer_2" name="fashion_design_layer_2">
                 </td>
             </tr>
             <tr class="form-field">
                 <th scope="row"><label for="fashion_design_layer_3">Upload Layer 3:</label></th>
-                <td><input type="file" id="fashion_design_layer_3" name="fashion_design_layer_3">
+                <td><input type="text" id="fashion_design_layer_3" name="fashion_design_layer_3">
                 </td>
             </tr>
             <tr class="form-field">
                 <th scope="row"><label for="fashion_design_layer_4">Upload Layer 4:</label></th>
-                <td><input type="file" id="fashion_design_layer_4" name="fashion_design_layer_4">
+                <td><input type="text" id="fashion_design_layer_4" name="fashion_design_layer_4">
                 </td>
             </tr>
             <tr class="form-field">
                 <th scope="row"><label for="fashion_design_layer_5">Upload Layer 5:</label></th>
-                <td><input type="file" id="fashion_design_layer_5" name="fashion_design_layer_5">
+                <td><input type="text" id="fashion_design_layer_5" name="fashion_design_layer_5">
                 </td>
             </tr>
             <tr class="form-field">
