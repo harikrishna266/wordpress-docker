754fae2de761411846da44346ff77bf418eb2f60
krishnajith <krishnajithr55@gmail.com>
2024-05-30 16:07:27 +0530

    feat: 🎸 added patterns edit
    added patterns edit
    
    ✅ Closes: #64

--- custom-plugins/fictive_studios/admin/patterns/api/patterns-api.php
+++ custom-plugins/fictive_studios/admin/patterns/api/patterns-api.php
@@ -5,11 +5,6 @@ namespace FictiveCodes;
 class PatternsAPIAdmin
 {
 
-    public function __construct()
-    {
-        add_filter('upload_mimes', array($this, 'custom_upload_svg'));
-    }
-
     public function get_all_patterns()
     {
         global $wpdb;
@@ -37,76 +32,65 @@ class PatternsAPIAdmin
         wp_die();
     }
 
-    public function custom_upload_svg($mimes)
-    {
-        $mimes['svg'] = 'image/svg+xml';
-        return $mimes;
-    }
-
     public function save_pattern_data()
     {
         $pattern_name = isset($_POST['pattern_name']) ? sanitize_text_field($_POST['pattern_name']) : '';
+        $pattern_image_url = isset($_POST['pattern_image']) ? sanitize_text_field($_POST['pattern_image']) : '';
+        $pattern_file_url = isset($_POST['pattern_file']) ? sanitize_text_field($_POST['pattern_file']) : '';
 
-        $pattern_file_url = $this->handle_file_submission('pattern_file');
-        $pattern_image_url = $this->handle_file_submission('pattern_image');
+        global $wpdb;
+        $patterns_table = FICTIVE_TABLE . 'patterns';
 
-        if ($pattern_file_url !== false && $pattern_image_url !== false) {
-            global $wpdb;
-            $patterns_table = FICTIVE_TABLE . 'patterns';
+        $insert_data = array(
+            'name' => $pattern_name,
+            'user' => get_current_user_id(),
+            'pattern_url' => $pattern_file_url,
+            'pattern_image' => $pattern_image_url
+        );
 
-            $insert_data = array(
+        $wpdb->insert($patterns_table, $insert_data, array('%s', '%d', '%s', '%s'));
+
+        $redirect_page_params = array(
+            'page' => PATTERNS_BUILDER_SLUG,
+        );
+        $url = add_query_arg($redirect_page_params, admin_url('admin.php'));
+        wp_redirect($url);
+    }
+
+    public function edit_pattern_data()
+    {
+        $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
+        $pattern_name = isset($_POST['pattern_name']) ? sanitize_text_field($_POST['pattern_name']) : '';
+        $pattern_image_url = isset($_POST['pattern_image']) ? sanitize_text_field($_POST['pattern_image']) : '';
+        $pattern_file_url = isset($_POST['pattern_file']) ? sanitize_text_field($_POST['pattern_file']) : '';
+
+        global $wpdb;
+        $table_name = FICTIVE_TABLE . 'patterns';
+
+        if ($id <= 0) {
+            wp_redirect(admin_url('admin.php?page=' . PATTERNS_BUILDER_SLUG . '&error=invalid_id'));
+            exit;
+        }
+        $wpdb->update(
+            $table_name,
+            array(
                 'name' => $pattern_name,
                 'user' => get_current_user_id(),
                 'pattern_url' => $pattern_file_url,
                 'pattern_image' => $pattern_image_url
-            );
+            ),
+            array('id' => $id),
+            array('%s', '%d', '%s', '%s'),
+            array('%d')
+        );
 
-            $wpdb->insert($patterns_table, $insert_data);
+        $redirect_params = array(
+            'page' => PATTERNS_BUILDER_SLUG,
+        );
 
-            $redirect_page_params = array(
-                'page' => PATTERNS_BUILDER_SLUG,
-            );
-            $url = add_query_arg($redirect_page_params, admin_url('admin.php'));
-            wp_redirect($url);
-        } else {
-            wp_die('Error uploading file');
-        }
-    }
-
-    public function handle_file_submission($file_name)
-    {
-        $files_url = '';
-        $file_upload_location = $file_name === 'pattern_file' ? 'custom_pattern_file_upload_dir' : 'custom_pattern_image_upload_dir';
-
-        if (!empty($_FILES[$file_name]['tmp_name'])) {
-            add_filter('upload_dir', array($this, $file_upload_location));
-            $uploaded_file = wp_handle_upload($_FILES[$file_name], array('test_form' => false));
-            remove_filter('upload_dir', array($this, $file_upload_location));
-            if (!isset($uploaded_file['error'])) {
-                $files_url = $uploaded_file['url'];
-            } else {
-                return false;
-            }
-        } else {
-            $files_url = '';
-        }
-
-        return $files_url;
-    }
-
-
-    public function custom_pattern_file_upload_dir($upload)
-    {
-        $upload['path'] = WP_CONTENT_DIR . '/uploads/patterns/files';
-        $upload['url'] = WP_CONTENT_URL . '/uploads/patterns/files';
-        return $upload;
-    }
-
-    public function custom_pattern_image_upload_dir($upload)
-    {
-        $upload['path'] = WP_CONTENT_DIR . '/uploads/patterns/images';
-        $upload['url'] = WP_CONTENT_URL . '/uploads/patterns/images';
-        return $upload;
+        $url = add_query_arg($redirect_params, admin_url('admin.php'));
+        wp_redirect($url);
+        exit;
     }
 
 }
--- custom-plugins/fictive_studios/admin/patterns/create/patterns-create.php
+++ custom-plugins/fictive_studios/admin/patterns/create/patterns-create.php
@@ -6,6 +6,19 @@ class PatternsCreate
     public function get_create_patterns_template()
     {
         $action = ($_GET['action']);
+        $pattern_data = null;
+        if ($action == 'edit') {
+            $pattern_data = $this->get_pattern_data_for_edit();
+        }
         require_once (plugin_dir_path(__FILE__) . '../create/patterns-create-view.php');
+    }
+
+    public function get_pattern_data_for_edit()
+    {
+        $pattern_id = ($_GET['pattern']);
+        global $wpdb;
+        $table_name = FICTIVE_TABLE . 'patterns';
+        $query = $wpdb->prepare("SELECT * FROM $table_name WHERE ID = %d", $pattern_id);
+        return $wpdb->get_row($query);
     }
 }
