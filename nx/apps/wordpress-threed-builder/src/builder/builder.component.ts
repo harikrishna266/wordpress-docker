edca6ab585fe8d49f9f11ab9e5c1907d864f5ed9
hari <harikrishna266@gmail.com>
2024-06-30 03:11:02 +0530

    decal first implementation

--- nx/apps/wordpress-threed-builder/src/builder/builder.component.ts
+++ nx/apps/wordpress-threed-builder/src/builder/builder.component.ts
@@ -10,7 +10,7 @@ import {
   loadModel,
   SceneHelper,
   DynamicTexture,
-  PBRMaterial
+  PBRMaterial, Mesh, Texture
 } from '@brocha-libs/builder-3d';
 import { HttpClientModule } from '@angular/common/http';
 import { Stage2D } from '@brocha-libs/builder-2d';
@@ -21,6 +21,14 @@ import { LayerPatternsComponent } from './layer-patterns/layer-patterns.componen
 import { Designs } from './types/design.type';
 import { Path } from '@brocha-libs/builder-2d/lib/shapes/path';
 import { LayerHelper } from './layer.helper';
+import {
+  Color3,
+  CreateDecal, Matrix, MeshBuilder,
+  PBRMetallicRoughnessMaterial, PickingInfo,
+  PointerEventTypes,
+  StandardMaterial,
+  Vector3
+} from '@babylonjs/core';
 type MenuActions =  'designs' | 'layers' | 'patterns' | 'none';
 
 @Component({
@@ -66,21 +74,104 @@ export class BuilderComponent implements AfterViewInit{
   async ngAfterViewInit() {
     this.stage = new Stage2D();
     this.stage.initializeStage(this.konvaContainer.nativeElement, 2048, 2048);
-    this.stage.isEditor = false;
+    this.stage.isEditor = true;
     await this.threeDBuilder();
   }
 
   async threeDBuilder() {
     await this.sceneHelper.createScene(this.threeDCanvas.nativeElement);
+    this.sceneHelper.scene.useRightHandedSystem = false;
     this.sceneHelper.addExternalEnvironment(`${environment.ASSET_URL}assets/environmentSpecular.env`);
     this.sceneHelper.loadCamera();
     await this.loadModel();
   }
 
   async loadModel() {
-    await loadModel(this.sceneHelper.scene, 'model', '', `${environment.ASSET_URL}glb/`, 'model-3.glb');
+    await loadModel(this.sceneHelper.scene, 'model', '', `${environment.ASSET_URL}glb/`, 'flip-model-3.glb');
+    (this.sceneHelper.scene.getMeshByName('bounding-box') as Mesh).isPickable = false;
+    ((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).bumpTexture = new Texture(`${environment.ASSET_URL}assets/Cotton_Heavy_Canvas_NRM.jpg`, this.sceneHelper.scene);
+    (((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).bumpTexture as Texture).level = 2;
+    (((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).bumpTexture as Texture).uScale = 5;
+    (((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).bumpTexture as Texture).vScale = 5;
+    ((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).metallic = 0.2;
+    ((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).roughness = .2;
+    ((this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).material as PBRMaterial).indexOfRefraction = 1.9;
     await this.renderDynamicTexture();
+    this.createDecal();
   }
+
+
+  createDecalMesh(position: Vector3, normal: Vector3, ) {
+    const decalMaterial = new PBRMaterial('decal', this.sceneHelper.scene);
+    decalMaterial.albedoColor = Color3.Red();
+    decalMaterial.zOffset = -30;
+    const decalMesh = CreateDecal('decal',
+        this.sceneHelper.scene.getMeshByName('Cloth') as Mesh,
+        {
+          position,
+          size: new Vector3(.1, .1, .1),
+          normal
+        });
+      decalMesh.material = decalMaterial;
+    ((this.sceneHelper.scene.getMeshByName('decal') as Mesh).material as PBRMaterial).metallic = 0.2;
+    ((this.sceneHelper.scene.getMeshByName('decal') as Mesh).material as PBRMaterial).roughness = .2;
+  }
+
+  createDecal() {
+    // (this.sceneHelper.scene.getMeshByName('Cloth') as Mesh).flipFaces(true);
+    //
+    // this.sceneHelper.scene.onPointerObservable.add((evtData, evtState) => {
+    //   const pickResult = this.sceneHelper.scene.pick(this.sceneHelper.scene.pointerX, this.sceneHelper.scene.pointerY, (mesh) => mesh.name !== 'bounding-box');
+    //   switch (evtData.type) {
+    //     case PointerEventTypes.POINTERUP:
+    //       console.log(pickResult);
+    //       if (pickResult && pickResult.hit) {
+    //         const normal = pickResult.getNormal();
+    //         const position = pickResult.pickedPoint;
+    //         if (normal && position) {
+    //            this.createDecalMesh(position, normal);
+    //         }
+    //       }
+    //       break;
+    //   }
+    // });
+    this.sceneHelper.scene.onPointerDown = () => {
+       const ray = this.sceneHelper.scene.createPickingRay(
+        this.sceneHelper.scene.pointerX,
+        this.sceneHelper.scene.pointerY,
+        Matrix.Identity(),
+        this.sceneHelper.scene.activeCamera
+      );
+
+      const raycastHit = this.sceneHelper.scene.pickWithRay(ray)  ;
+      console.log(raycastHit);
+
+      if (raycastHit && raycastHit.hit && raycastHit.pickedMesh && raycastHit.pickedMesh.name === "Cloth") {
+        const decal = MeshBuilder.CreateDecal("decal", raycastHit.pickedMesh, {
+          position: raycastHit.pickedPoint as Vector3,
+          normal: raycastHit.getNormal(true) as Vector3,
+          size: new Vector3(.1, .1, .1),
+        });
+        const material = new PBRMaterial('decal', this.sceneHelper.scene);
+        material.albedoTexture = this.dynamicTexture;
+        material.metallic = 0.2;
+        material.roughness = 0.2;
+        decal.material = material;
+      }
+
+        // decal.material =
+        //   this.splatters[Math.floor(Math.random() * this.splatters.length)];
+        //
+        // decal.setParent(raycastHit.pickedMesh);
+        //
+        // raycastHit.pickedMesh.physicsImpostor.applyImpulse(
+        //   ray.direction.scale(5),
+        //   raycastHit.pickedPoint
+        // );
+
+    }
+  }
+
 
   async renderDynamicTexture() {
     this.dynamicTexture = await createDynamicTexture('main-texture',
@@ -93,6 +184,22 @@ export class BuilderComponent implements AfterViewInit{
         material.albedoTexture = this.dynamicTexture;
       }
     });
+  }
+
+  async addRect() {
+    const rectangle = this.stage.createShape('rect');
+    await rectangle.setAttrs({
+      width: 300,
+      height: 300,
+      x: 372,
+      y: 600,
+      fill: 'yellow'
+    });
+    rectangle.shape.zIndex(5);
+    await this.stage.addShape( this.stage.layer, rectangle);
+    await this.stage.layer.draw();
+    this.dynamicTexture.update(false);
+
   }
 
 }
