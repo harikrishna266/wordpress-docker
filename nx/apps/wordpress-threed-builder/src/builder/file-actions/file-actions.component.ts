a58d78322a148ffa602a44364696bb2c644f8f52
Krishnajith <krishnajithr55@gmail.com>
2024-07-05 16:00:29 +0530

    feat: 🎸 design flow

---
+++ nx/apps/wordpress-threed-builder/src/builder/file-actions/file-actions.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import { WordpressService } from '../../services/wordpress.service';
+import { tap } from 'rxjs';
+
+type UserActions = 'save' | 'cancel';
+
+@Component({
+  selector: 'app-file-actions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './file-actions.component.html',
+  styleUrl: './file-actions.component.css',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FileActionsComponent {
+  constructor() {}
+
+  private wordpressService = inject(WordpressService);
+  @Input() designData: any = [];
+
+  saveDesign() {
+    this.wordpressService.saveDesings(this.designData).subscribe();
+  }
+}
