bd47622251ce4f099515ef054de39324e824a61f
Krishnajith <krishnajithr55@gmail.com>
2024-07-05 20:07:05 +0530

    fix: 🐛 added global variable for wordpress uel in angular apis

--- nx/apps/wordpress-threed-builder/src/services/wordpress.service.ts
+++ nx/apps/wordpress-threed-builder/src/services/wordpress.service.ts
@@ -9,19 +9,18 @@ import { Observable } from 'rxjs';
 })
 export class WordpressService {
 
+  $wordpressURL: string = '/wp-admin/admin-ajax.php'; 
+
   private readonly http: HttpClient = inject(HttpClient);
   getDesigns() {
-      return this.http.get<Designs[]>('/wp-admin/admin-ajax.php?action=get_all_designs')
+      return this.http.get<Designs[]>(`${this.$wordpressURL}?action=get_all_designs`)
   }
 
   getAllPatterns() {
-    return this.http.get<Pattern[]>('/wp-admin/admin-ajax.php?action=get_all_patterns')
+    return this.http.get<Pattern[]>(`${this.$wordpressURL}?action=get_all_patterns`)
   }
 
   saveDesings(designData:any): Observable<any[]>{
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-    return this.http.post<any[]>('/wp-admin/admin-ajax.php?action=save_user_design', designData, { headers: headers });
+    return this.http.post<any[]>(`${this.$wordpressURL}?action=save_user_design`, designData);
   }
 }
